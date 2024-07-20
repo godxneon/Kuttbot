21fa816ba630e94977a081494416a740d3898f2b
HacKer Jr ~ TG <83758692+godxneon@users.noreply.github.com>
2024-07-21 02:12:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/GouthamSER/KuttuBot.git /KuttuBot
+  git clone https://github.com/godxneon/Kuttbot.git /Kuttbot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /KuttuBot
+  git clone $UPSTREAM_REPO /Kuttbot
 fi
-cd /KuttuBot
+cd /Kuttbot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
